22a005bfb247617e4a7fe53f7cd28a0b0415f33e
Xin Zhao <zhaoxina@gmail.com>
2015-02-11 13:55:55 +0800

    add comment.sh

--- src/main/java/com/xin/LeetCodeAnswers/ReverseWordsInAString.java
+++ src/main/java/com/xin/LeetCodeAnswers/ReverseWordsInAString.java
@@ -1,5 +1,31 @@
 package com.xin.LeetCodeAnswers;
 
+// Source : https://oj.leetcode.com/problems/reverse-words-in-a-string/
+// Author : Xin Zhao
+// Date   : 2015-02-11
+
+/**********************************************************************************
+ *
+ * Given an input string, reverse the string word by word.
+ *
+ * For example,
+ * Given s = "the sky is blue",
+ * return "blue is sky the".
+ *
+ * click to show clarification.
+ *
+ * Clarification:
+ *
+ * What constitutes a word?
+ * A sequence of non-space characters constitutes a word.
+ * Could the input string contain leading or trailing spaces?
+ * Yes. However, your reversed string should not contain leading or trailing spaces.
+ * How about multiple spaces between two words?
+ * Reduce them to a single space in the reversed string.
+ *
+ *
+ **********************************************************************************/
+
 public class ReverseWordsInAString {
 	
     public static String reverseWords(String s) {
