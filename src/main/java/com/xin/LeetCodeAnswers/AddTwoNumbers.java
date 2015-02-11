33f548502e02e83816036692fd9ca4820f30adee
Xin Zhao <zhaoxina@gmail.com>
2015-02-11 15:56:27 +0800

    add problem description

--- src/main/java/com/xin/LeetCodeAnswers/AddTwoNumbers.java
+++ src/main/java/com/xin/LeetCodeAnswers/AddTwoNumbers.java
@@ -1,5 +1,17 @@
 package com.xin.LeetCodeAnswers;
 
+// Source : https://oj.leetcode.com/problems/add-two-numbers/
+// Author : Xin Zhao
+// Date   : 2015-02-11
+
+/**********************************************************************************
+ *
+ * You are given two linked lists representing two non-negative numbers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+ *
+ * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * Output: 7 -> 0 -> 8
+ *
+ **********************************************************************************/
 
 public class AddTwoNumbers {
 	
--- src/main/java/com/xin/LeetCodeAnswers/EvaluateReversePolishNotation.java
+++ src/main/java/com/xin/LeetCodeAnswers/EvaluateReversePolishNotation.java
@@ -1,5 +1,23 @@
 package com.xin.LeetCodeAnswers;
 
+// Source : https://oj.leetcode.com/problems/evaluate-reverse-polish-notation/
+// Author : Xin Zhao
+// Date   : 2015-02-11
+
+/**********************************************************************************
+ *
+ * Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+ *
+ * Valid operators are +, -, *, /. Each operand may be an integer or another expression.
+ *
+ * Some examples:
+ *
+ *   ["2", "1", "+", "3", "*"] -> ((2 + 1) * 3) -> 9
+ *   ["4", "13", "5", "/", "+"] -> (4 + (13 / 5)) -> 6
+ *
+ *
+ **********************************************************************************/
+
 import java.util.Stack;
 
 public class EvaluateReversePolishNotation {
